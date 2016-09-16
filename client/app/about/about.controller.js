3d322ba403c3057c88e1ac2bd1f5a674387583da
Joey Gauthier <Paul.Gauthier@tolling.telvent.com>
2016-09-16 14:33:21 -0500

    more touches on projects, setting up about, better svg

---
+++ client/app/about/about.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('portfolioApp').controller('AboutCtrl', ['$rootScope', '$scope', 'Dataobjects', '$mdDialog', '$timeout', '$http', '$compile',
+	function ($rootScope, $scope, Dataobjects, $mdDialog, $timeout, $http, $compile) {
+
+	}
+]);
