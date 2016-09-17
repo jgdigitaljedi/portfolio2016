ff89cc8cc2d89895d3941c398abd75afec1a2baf
Joey Gauthier <digitaljedi@outlook.com>
2016-09-17 07:22:01 -0500

    social network buttons in shell nav

--- client/app/app.js
+++ client/app/app.js
@@ -48,7 +48,43 @@ angular.module('portfolioApp', [
     $mdThemingProvider.enableBrowserColor({theme: 'night'});
 }).controller('AppCtrl', function ($scope, $rootScope) {
     var theHour = parseInt(moment().format('HH'));
-    console.log('theHour',theHour);
     $scope.theme = (theHour >= 7 && theHour < 19) ? 'day' : 'night';
     $rootScope.theme = $scope.theme;
+
+    // social network button garbage
+    //Facebook
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    // Twitter
+    window.twttr = (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+      if (d.getElementById(id)) return t;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://platform.twitter.com/widgets.js';
+      fjs.parentNode.insertBefore(js, fjs);
+     
+      t._e = [];
+      t.ready = function(f) {
+        t._e.push(f);
+      };
+     
+      return t;
+    }(document, 'script', 'twitter-wjs'));
+
+    // Google+
+    (function() {
+      var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+      po.src = 'https://apis.google.com/js/platform.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    })();
 });
