f2be337c0bb7cf5f406a4b2cc2b6b3e056116c7a
Joey Gauthier <digitaljedi@outlook.com>
2016-09-18 08:44:29 -0500

    experimenting on about page

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -1,13 +1,42 @@
 'use strict';
 
-angular.module('portfolioApp').controller('AboutCtrl', ['$rootScope',
-	function ($rootScope) {
-		var ac = this;
+angular.module('portfolioApp').controller('AboutCtrl', ['$scope','$rootScope', '$interval',
+	function ($scope, $rootScope, $interval) {
+		var ac = this,
+			photoInt,
+			nextIndex;
         ac.showLastfm = true;
         ac.theme = $rootScope.theme;
+        ac.photoArr = ['../assets/images/about/meDsShow.jpg', '../assets/images/about/coachingSoccer.jpg',
+        	'../assets/images/about/meGuitarShoot.jpg'];
+        ac.photo = ac.photoArr[0];
+        var paLen = ac.photoArr.length;
 
         $rootScope.$on('theme change', function () {
             ac.theme = $rootScope.theme;
+        });
+
+        (function init () {
+        	photoInt = $interval(function () {
+        			console.log('interval');
+        		ac.photoArr.forEach(function (item, index) {
+        			if (ac.photo === item) {
+        				if (index + 1 === paLen) {
+        					nextIndex = 0;
+        					return;
+        				} else {
+        					nextIndex = index + 1;
+        				console.log('ac.photo', ac.photo);
+        					return;
+        				}
+        			}
+        		});
+        		ac.photo = ac.photoArr[nextIndex];
+        	}, 5000);
+        })();
+
+        $scope.$on('$destroy', function () {
+        	$interval.cancel(photoInt);
         });
 	}
 ]);
