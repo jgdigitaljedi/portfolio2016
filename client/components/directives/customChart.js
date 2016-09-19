431b3b0e5f3c5e2bb89446a36868419be23c839a
Joey Gauthier <digitaljedi@outlook.com>
2016-09-19 08:23:28 -0500

    about page layout done, styling time ahead

--- client/components/directives/customChart.js
+++ client/components/directives/customChart.js
@@ -8,26 +8,29 @@ angular.module('portfolioApp').directive('skillChart', ['Dataobjects', '$compile
             transclude: true,
             scope: {
                 skillData: '=',
-                chartWidth: '='
+                chartWidth: '=',
+                chartHeight: '='
             },
             link: function(scope, elem) {
 
                 function createChart () {
-                    var template = '',
+                    var template = '<div class="custom-chart-container" layout="row" layout-align="space-around end" '+
+                            'style="width: 100%; height: ' + scope.chartHeight + 'px;">',
                         colors = Dataobjects.getMaterialColors(),
                         cLen = colors.length,
                         skillsLen = scope.skillData.skillList.length,
                         barWidth = scope.chartWidth / ((skillsLen*2) + 1);
                     
                     scope.skillData.skillList.forEach(function (item) {
-                        template += '<div layout="column" layout-align="center center"><div>' + item.rating + '%</div>'+
-                            '<div class="chart-bar" style="background-color: '+
-                            colors[Math.floor(Math.random() * cLen)] +'; height: ' + 320 * (item.rating / 100) +
+                        template += '<div layout="column" layout-align="center center"><div>' + 
+                            item.rating + '%</div><div class="chart-bar" style="background-color: '+
+                            colors[Math.floor(Math.random() * cLen)] +'; height: ' + scope.chartHeight * (item.rating / 100) +
                             'px; width: ' + barWidth + 'px;" layout="column">'+
                             '</div>'+
                                 '<span>' + item.name + '</span>'+
                             '</div>';
-                    }); 
+                    });
+                    template += '</div>';
                     elem.html($compile(template)(scope));                 
                 }
 
