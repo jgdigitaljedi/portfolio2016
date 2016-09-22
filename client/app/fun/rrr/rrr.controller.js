5305f98eeebb0d35bb48bc36817c881ddf3e6526
Joey Gauthier <digitaljedi@outlook.com>
2016-09-21 22:04:33 -0500

    a bit more rrrr work

--- client/app/fun/rrr/rrr.controller.js
+++ client/app/fun/rrr/rrr.controller.js
@@ -8,7 +8,8 @@ angular.module('portfolioApp').controller('RrrCtrl', ['$scope', '$rootScope', '$
         		longWalk: 1200, // 15 minutes
         		shortDrive: 8046, // 5 miles
         		noPreference: 80467 // 50 miles
-        	};
+        	},
+        	resultsLen;
         rrrc.theme = $rootScope.theme;
         console.log('state', $state.current);
         if ($state.current.name === 'rrr') {
@@ -34,6 +35,7 @@ angular.module('portfolioApp').controller('RrrCtrl', ['$scope', '$rootScope', '$
         rrrc.getCurrentLocation = function () {
         	Geolocation.getCurrentPosition().then(function (data) {
         		callYelp(data.location.coords.latitude, data.location.coords.longitude);
+        		rrrc.step = 'first';
         		$state.go('rrr.options');
         	});
         };
@@ -47,48 +49,82 @@ angular.module('portfolioApp').controller('RrrCtrl', ['$scope', '$rootScope', '$
 			if (choice) {
 				rrrc.filteredChoices = [];
 				rrrc.categories = [];
-				rrrc.selectedCategories = {};
+				rrrc.selectedCategories = {list: []};
 				var limit = distances[choice];
 				rrrc.restaurantChoices.forEach(function (item) {
 					if (item.distance <= limit) {
 						rrrc.filteredChoices.push(item);
 						if (item.categories.length > 0) {
-							item.categories.forEach(function (ite, idx) {
-								console.log('cat', rrrc.categories.indexOf(ite));
+							item.categories.forEach(function (ite) {
 								if (rrrc.categories.length === 0) {
-									rrrc.categories.push(ite[0])
-									rrrc.selectedCategories[ite] = false;
+									rrrc.categories.push(ite[0]);
+									rrrc.selectedCategories[ite[0]] = false;
+									if (!rrrc.selectedCategories.list[ite[0]]) {
+										rrrc.selectedCategories.list[ite[0]] = [];
+									}
 								}
 								if (rrrc.categories.indexOf(ite[0]) === -1) {
 									rrrc.categories.push(ite[0]);
-									rrrc.selectedCategories[ite] = false;
+									rrrc.selectedCategories[ite[0]] = false;
+									if (!rrrc.selectedCategories.list[ite[0]]) {
+										rrrc.selectedCategories.list[ite[0]] = [];
+									}
 								}
+								rrrc.selectedCategories.list[ite[0]].push(item);
 							});
 						}
 					}
 				});
-				console.log('categories', rrrc.categories);
-				console.log('distance choices', rrrc.filteredChoices);
-				rrrc.showStep2 = true;		
+				rrrc.step = 'second';	
 			}
 		};
 
 		rrrc.doneWithCats = function () {
-			var catArr = [];
+			console.log('rrrc.selectedCategories', rrrc.selectedCategories);
+			var catArr = [],
+				rChoices = [];
+			rrrc.rChoicesNoDupes = [];
 			for (var cat in rrrc.selectedCategories) {
 				if (rrrc.selectedCategories[cat]) {
 					catArr.push(cat);
 				}
 			}
 			console.log('catArr', catArr);
-			// rrrc.filteredChoices.forEach(function (item) {
-
-			// });
-			console.log('rrrc.selectedCategories', rrrc.selectedCategories);
+			catArr.forEach(function (item) {
+				rChoices.push(rrrc.selectedCategories.list[item]);
+			});
+			console.log('rChoices', rChoices);
+			rChoices.forEach(function (item) {
+				if (item && item.length > 0) {
+					item.forEach(function (itm) {
+						if (rrrc.rChoicesNoDupes.length === 0) {
+							rrrc.rChoicesNoDupes.push({[itm.id]: itm});
+						} else if (!rrrc.rChoicesNoDupes.hasOwnProperty(item.id)) {
+							rrrc.rChoicesNoDupes.push({[itm.id]: itm});
+						}
+					});					
+				}
+			});
+			console.log('no dupes', rrrc.rChoicesNoDupes);
+			resultsLen = rrrc.rChoicesNoDupes.length;
+			rrrc.step = 'last';
 		};
 
+		function getRandomPlace (first) {
+			if (!first) {
+				return rrrc.rChoicesNoDupes[Math.floor(Math.random() * resultsLen)];
+			}
+		}
+
 		rrrc.showMeTheMoney = function () {
-			console.log('send pressed');
+			rrrc.finalAnswer = [];
+			if (rrrc.finalAnswer.length === 0) {
+				rrrc.finalAnswer.push(getRandomPlace());
+			}
+			// if (rrrc.userChoices === 2) {
+			// 	var nextChoice = getRandomPlace();
+			// 	if 
+			// }
 		};
     }
 ]);
